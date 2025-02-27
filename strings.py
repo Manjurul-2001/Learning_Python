8e90b4328fc8c817786e318a09f0601f86f13421
Md. Manjurul Islam <manjurul.apmat.du@gmail.com>
2025-02-27 19:26:25 +0600

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,25 @@
+course_1 = "Python's Course for Beginners."
+course_2 = 'Machine learning for "Beginners."'
+course_3 = '''
+Hi John,
+Here is my first email to you.
+
+thank You,
+The Support Team.
+'''
+print(course_1 , course_2)
+print(course_1[0], course_1[8])
+print(course_1[-1])
+print(course_1[0:6])
+print(course_1[7:11])
+print(course_1[1:])
+print(course_1[:6])
+print(course_3) 
+
+course = 'Python for beginners.'
+another = course[:]
+print (another)
+
+#Solution of problem given by Mosh
+name = 'Jennifer'
+print(name[1:-1])
