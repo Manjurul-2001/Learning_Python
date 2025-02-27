6e0906bdc5e17a12932792fb0b74951923b05451
Md. Manjurul Islam <manjurul.apmat.du@gmail.com>
2025-02-27 19:25:49 +0600

    Update type_conversion.py

--- type_conversion.py
+++ type_conversion.py
@@ -1,1 +1,12 @@
-print("Manjurul")
+'''
+birth_year = input('What is your birth year? : ')
+print("birth_year type :", type(birth_year))
+age = 2025 - int(birth_year)
+print("Currently, your are", age, "years old.")
+print("Age tpye :", type(age))
+'''
+
+#Problem solution given by Mosh
+weight_kg = input("Enter your weight in kg : ")
+weight_pound = float(weight_kg) * 2.20462 
+print("Your weight is", weight_pound, "lbs.")
